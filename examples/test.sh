cd5eba9f57f9881a15e073c3bb2e0befb4b23d21
Libor Polčák <ipolcak@fit.vutbr.cz>
2017-03-31 09:01:49 +0200

    Added autotesting of Ph.D. thesis examples
    Tests are repeatable, so that future testing can be more comfortable.

---
+++ examples/test.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+
+TMPFILE=`mktemp`
+
+echo "PPP_RADIUS: -s 1 RadiusLogin: JohnDoe"
+../linking.py -g ppp_radius.gml -s 1 "RadiusLogin: JohnDoe" > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv4: 10.0.0.1
+IPv4: 10.11.12.1
+IPv6: 2001:db8::1
+MAC: aa:bb:cc:00:11:22
+COMPARE
+
+echo "PPP_RADIUS:  -s 1 RadiusLogin: JohnDoe -t 2017-01-01T12:55"
+../linking.py -g ppp_radius.gml -s 1 "RadiusLogin: JohnDoe" -t 2017-01-01T12:55 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv4: 10.0.0.1
+IPv4: 10.11.12.1
+MAC: aa:bb:cc:00:11:22
+COMPARE
+
+echo "PPP_RADIUS: IPv4: 10.0.0.1 -s 1"
+../linking.py -g ppp_radius.gml "IPv4: 10.0.0.1" -s 1 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+COMPARE
+
+echo "PPP_RADIUS: IPv4: 10.0.0.1 -s 2"
+../linking.py -g ppp_radius.gml "IPv4: 10.0.0.1" -s 2 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv6: 2001:db8::1
+MAC: aa:bb:cc:00:11:22
+RadiusLogin: JohnDoe
+COMPARE
+
+echo "PPP_RADIUS: RadiusLogin: JohnDoe -s 3"
+../linking.py -g ppp_radius.gml "RadiusLogin: JohnDoe" -s 3 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv4: 10.0.0.1
+IPv4: 10.11.12.1
+IPv6: 2001:db8::1
+MAC: aa:bb:cc:00:11:22
+MAC: aa:bb:cc:55:66:77
+PPPLogin: JohnDoe
+COMPARE
+
+
+echo "IRC: IRC nickname: Bob -s 3"
+../linking.py -g irc.gml "IRC nickname: Bob" -s 3 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv4: 10.0.0.1
+TCP: 10.0.0.1:1234 <-> 172.16.1.7:7000
+COMPARE
+
+
+echo "IRC: IRC channel: foo -s 4"
+../linking.py -g irc.gml "IRC channel: foo" -s 4 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IRC nickname: Alice
+IRC nickname: Bob
+COMPARE
+
+
+echo "IRC: IPv4: 192.168.1.1 -s 5"
+../linking.py -g irc.gml "IPv4: 192.168.1.1" -s 5 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IRC nickname: Alice
+COMPARE
+
+
+echo "IRC: IPv4: 192.168.1.1 -s 6"
+../linking.py -g irc.gml "IPv4: 192.168.1.1" -s 6 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IRC channel: foo
+COMPARE
+
+
+echo "Inaccuracy: 5"
+../linking.py -g inaccuracy.gml "IPv6: 2001:db8::1" -i 5 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+Clock skew: 23.7 +- 0.5 ppm
+Clock skew: 23.9 +- 0.8 ppm
+IPv6: 2001:db8::2
+COMPARE
+
+
+echo "Inaccuracy: 10"
+../linking.py -g inaccuracy.gml "IPv6: 2001:db8::1" -i 10 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+Clock skew: 23.7 +- 0.5 ppm
+Clock skew: 23.9 +- 0.8 ppm
+IPv6: 2001:db8::2
+IPv6: 2001:db8::3
+COMPARE
+
+
+echo "Inaccuracy: 11"
+../linking.py -g inaccuracy.gml "IPv6: 2001:db8::1" -i 11 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+Clock skew: 23.7 +- 0.5 ppm
+Clock skew: 23.9 +- 0.8 ppm
+Clock skew: 25.3 +- 1.0 ppm
+IPv6: 2001:db8::2
+IPv6: 2001:db8::3
+COMPARE
+
+
+echo "NAT: broken"
+../linking.py -g nat_broken.gml "SIP account: Alice" -s 3 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv4: 1.2.3.4
+TCP: 1.2.3.4:11223 <-> 11.12.13.14:80
+TCP: 1.2.3.4:1234 <-> 147.229.1.1:5060
+TCP: 10.0.0.1:1234 <-> 147.229.1.1:5060
+TCP: 10.0.0.2:11223 <-> 11.12.13.14:80
+UDP: 1.2.3.4:5678 <-> 5.6.7.8:2233
+UDP: 10.0.0.1:5678 <-> 5.6.7.8:2233
+COMPARE
+
+
+echo "NAT: fixed"
+../linking.py -g nat_fixed.gml "SIP account: Alice" -s 3 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+IPv4: 10.0.0.1
+TCP: 1.2.3.4:1234 <-> 147.229.1.1:5060
+TCP: 10.0.0.1:1234 <-> 147.229.1.1:5060
+UDP: 1.2.3.4:5678 <-> 5.6.7.8:2233
+UDP: 10.0.0.1:5678 <-> 5.6.7.8:2233
+COMPARE
+
+
+echo "NAT: IPv4: 1.2.3.4 (translator)"
+../linking.py -g nat_fixed.gml "IPv4: 1.2.3.4" -s 1 > "$TMPFILE"
+
+diff "$TMPFILE" - <<- COMPARE
+TCP: 1.2.3.4:11223 <-> 11.12.13.14:80
+TCP: 1.2.3.4:1234 <-> 147.229.1.1:5060
+TCP: 10.0.0.1:1234 <-> 147.229.1.1:5060
+TCP: 10.0.0.2:11223 <-> 11.12.13.14:80
+UDP: 1.2.3.4:5678 <-> 5.6.7.8:2233
+UDP: 10.0.0.1:5678 <-> 5.6.7.8:2233
+COMPARE
+
+rm "$TMPFILE"
