86e94ae31041a89589ed8327dc455790fc6da477
Libor Polčák <ipolcak@fit.vutbr.cz>
2017-07-21 15:26:21 +0200

    scopes: scope explanation added to help text

--- linking.py
+++ linking.py
@@ -101,17 +101,37 @@ def linked(g, i0, add_start, constraints):
         accepted_paths.append([i0])
     return set([p[-1] for p in accepted_paths])
 
+scope_help = """The linking scope (1-6):
+1~ Constraints revealing components of partial identity aka
+   Other corresponding identifiers
+2~ Constraints revealing partial identities of specific computer aka
+   Identifiers of a specific computer
+3~ Constraints revealing partial identities of computers where specific
+   user authenticated or logged in
+4~ Constraints revealing identifiers of all users accessing specific resource
+5~ Constraints revealing all user accounts logged in or authenticated from
+   computer or set of computers
+6~ Constraints revealing all accessed resources 
+"""
+
+timescope_help = """Time scope (1-2):
+1~ All edges on the path have to be valid during the whole period.
+2~ All edges on the path have to be valid at least once during the period
+   [-b, -e] and the period during the previous identifier is valid on the
+   path. 
+"""
+
 # Argument handling
 def process_args():
     parser = argparse.ArgumentParser(description="Identity linking software")
     parser.add_argument("--graph_file", "-g", help="Input graph file with identities.")
     parser.add_argument("inputid", help="The input id (type: id).", default=None)
     parser.add_argument("--scope", "-s", type=int, choices = [1,2,3,4,5,6],
-            help="The linking scope (1-6).", default=None)
+            help=scope_help, default=None)
     parser.add_argument("--begintime", "-b", type=TimeWrapper, help="Begin time for which to perform linkage (local TZ).")
     parser.add_argument("--endtime", "-e", type=TimeWrapper, help="End time for which to perform linkage (local TZ).")
     parser.add_argument("--timescope", "-t", type=int, choices = [1,2],
-            help="Time scope (1-2).", default=None)
+            help=timescope_help, default=None)
     parser.add_argument("--max_inaccuracy", "-i", type=float, help="Maximal path inaccuracy.")
     parser.add_argument("--components", "-c", action="store_true", help="Compute the number of components in the graph.")
     parser.add_argument("--add_self", "-a", action="store_true", help="Add the input node to the output set")
